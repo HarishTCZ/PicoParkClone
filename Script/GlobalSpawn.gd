37fc11352d5678e404b5029a4a3e4b68e3d49a8b
Harish Ragavendra <kumardeepaktc@gmail.com>
2024-05-25 22:11:47 +0530

    added game zip file

---
+++ Script/GlobalSpawn.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+var spawn_point = Vector2(0,0)
+
+func update_spawn(new_point):
+	spawn_point = new_point
--- Script/game_manager.gd
+++ Script/game_manager.gd
@@ -1,8 +1,9 @@
 extends Node
 
-
+@onready var pause = $PauseMenu
 var points = 0
 var key = 0
+
 
 func add_point():
 	points +=1 
@@ -11,3 +12,11 @@ func add_point():
 func add_key():
 	key +=1
 	
+func _input(event):
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	
+	if Input.is_action_just_pressed("pause"):
+		var current_value : bool = get_tree().paused
+		get_tree().paused = not current_value
+		
